fac4e505ba64a07ac0110a73232f528a3f133b35
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-04 15:02:02 +0100

    feat: exercise completed, creating new variables to modify html and turn on the lamp

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('ciao sono una lampadina');
+//salvo l'elemento con la lampadina spenta in una variabile cercandola per Id
+const lampOffElement = document.getElementById('lamp-off');
+
+//salvo l'elemento pulsante in una variabile
+const buttonOn = document.querySelector('button');
+
+//faccio in modo che, una volta cliccato il pulsante, l'immagine della lampadina spenta venga sostituita da quella accesa
+buttonOn.addEventListener('click',
+    function(){
+        lampOffElement.src = 'img/yellow_lamp_fix.png';
+        lampOffElement.alt = 'lampadina-accesa';
+    }
+)
