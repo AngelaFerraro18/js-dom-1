495de120ebca410af4029527b85983369dacda48
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-04 16:45:43 +0100

    feat: bonus completed with lamp turn on and off every time btn is clicked

---
+++ esercizio-bonus/js-bonus/app-bonus.js
@@ -0,0 +1,26 @@
+//salvo l'elemento con la lampadina spenta in una variabile cercandola per Id
+const lampOffElement = document.getElementById('lamp-off');
+console.log(lampOffElement);
+
+//salvo l'elemento pulsante in una variabile
+const buttonOn = document.querySelector('button');
+
+
+
+//faccio in modo che, una volta cliccato il pulsante, l'immagine della lampadina spenta venga sostituita da quella accesa e viceversa
+buttonOn.addEventListener('click',
+    function(){
+        
+     if (buttonOn.innerText.includes('Accendi la lampadina!')){
+        lampOffElement.src = '../img/yellow_lamp_fix.png';
+        lampOffElement.alt = 'lampadina-accesa';
+        buttonOn.innerText = 'Spegni la lampadina!';
+        
+     } else {
+        lampOffElement.src = '../img/white_lamp.png';
+        buttonOn.textContent = 'Accendi la lampadina!';
+     }
+    } 
+)
+
+
